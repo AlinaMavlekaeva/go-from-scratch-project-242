7964b4ed781c2a6fa61e441d32fd3580044fe0e4
Alina_Mavlekaeva <alinamavlekaeva@gmail.com>
2026-04-10 18:20:06 +0300

    bag: unworking func is added

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -3,7 +3,22 @@ import (
 "os"
 "context"
 "github.com/urfave/cli/v3"
+"fmt"
+"code"
 )
 func main() {
-(&cli.Command{}).Run(context.Background(), os.Args)
+cmd := &cli.Command{
+Name:   "hexlet-path-size"
+Usage:  "Get path's size"
+Action: func(ctx context.Context, cmd *cli.Command) error {
+fileName := cmd.Args().Get(0)
+fileSize := code.GetPathSize(fileName)
+result := fmt.Sprintf("%s\t%s", fileSize, fileName)
+fmt.Println(result)
+return nil
+        },
+}
+if err := cmd.Run(context.Background(), os.Args); err != nil {
+log.Fatal(err)
+        }
 }
