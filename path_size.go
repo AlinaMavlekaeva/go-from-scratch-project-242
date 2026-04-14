71c365b08e5a05abe91f01cbc2565c4f237f3a54
Alina_Mavlekaeva <alinamavlekaeva@gmail.com>
2026-04-14 17:48:08 +0300

    feat: human readable format added

--- path_size.go
+++ path_size.go
@@ -3,20 +3,41 @@ import (
 "os"
 "fmt"
 )
-func GetPathSize(filePath string) (string, error) {
-info, err := os.Lstat(filePath)
+func ConvertSize(size int64) string {
+units := map[int]string{
+0: "B",
+1: "KB",
+2: "MB",
+3: "GB",
+4: "TB",
+5: "PB",
+6: "EB",
+}
+if size < 1024 {
+return fmt.Sprintf("%d%s", size, units[0])
+}
+size = size / 1024
+for i := 1; i < 6; i++ {
+if size < 1024 {
+return fmt.Sprintf("%d%s", size, units[i])
+}
+size = size / 1024
+}
+return fmt.Sprintf("%d%s", size, units[6])
+}
+func GetPathSize(path string) (string, error) {
+info, err := os.Lstat(path)
 if err != nil {
 return "", err
 }
 if info.IsDir() {
-entries, _ :=os.ReadDir(filePath)
+entries, _ :=os.ReadDir(path)
 var dirSize int64 = 0
 for _, entry := range entries {
 entryInfo, _ := entry.Info()
 dirSize += entryInfo.Size()
 }
-return fmt.Sprintf("%dB", dirSize), nil
-} else {
-return fmt.Sprintf("%dB", info.Size()), nil
+return ConvertSize(dirSize), nil
 }
+return ConvertSize(info.Size()), nil
 }
