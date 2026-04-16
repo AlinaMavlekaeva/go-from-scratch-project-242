1052b10adcc2c6770044fadbc854a4b149424ea7
Alina_Mavlekaeva <alinamavlekaeva@gmail.com>
2026-04-16 12:01:12 +0300

    feat: 'all' flag (include hidden files and directories) has been added

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,33 +1,42 @@
 package main
+
 import (
-"os"
-"context"
-"github.com/urfave/cli/v3"
-"fmt"
-"code"
+	"code"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v3"
 )
+
 func main() {
-cmd := &cli.Command {
-Flags: []cli.Flag {
-&cli.BoolFlag {
- Name: "human",
- Aliases: []string{"H"},
-Value: true,
- Usage: "human-readable sizes (auto-select unit)",
-},
-},
-Action: func(ctx context.Context, cmd *cli.Command) error {
-fileName := cmd.Args().Get(0)
-fileSize, err := code.GetPathSize(fileName, cmd.Bool("human"))
-if err != nil {
-return err
-}
-result := fmt.Sprintf("%s\t%s", fileSize, fileName)
-fmt.Println(result)
-return nil
-},
-}
-if err := cmd.Run(context.Background(), os.Args); err != nil {
-fmt.Println("Ошибка: ", err)
-}
+	cmd := &cli.Command{
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    "human",
+				Aliases: []string{"H"},
+				Value:   true,
+				Usage:   "human-readable sizes (auto-select unit)",
+			},
+			&cli.BoolFlag{
+				Name:    "all",
+				Aliases: []string{"a"},
+				Value:   false,
+				Usage:   "include hidden files and directories",
+			},
+		},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			fileName := cmd.Args().Get(0)
+			fileSize, err := code.GetPathSize(fileName, cmd.Bool("human"), cmd.Bool("all"))
+			if err != nil {
+				return err
+			}
+			result := fmt.Sprintf("%s\t%s", fileSize, fileName)
+			fmt.Println(result)
+			return nil
+		},
+	}
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Println("Ошибка: ", err)
+	}
 }
--- path_size.go
+++ path_size.go
@@ -1,43 +1,64 @@
 package code
+
 import (
-"os"
-"fmt"
+	"fmt"
+	"os"
+	"strings"
 )
-func ConvertSize(size int64, flag bool) string {
-units := map[int]string{
-0: "B",
-1: "KB",
-2: "MB",
-3: "GB",
-4: "TB",
-5: "PB",
-6: "EB",
+
+func ConvertSize(size int64, convert bool) string {
+	units := map[int]string{
+		0: "B",
+		1: "KB",
+		2: "MB",
+		3: "GB",
+		4: "TB",
+		5: "PB",
+		6: "EB",
+	}
+	if !convert || size < 1024 {
+		return fmt.Sprintf("%d%s", size, units[0])
+	}
+	size = size / 1024
+	for i := 1; i < 6; i++ {
+		if size < 1024 {
+			return fmt.Sprintf("%.1f%s", float64(size), units[i])
+		}
+		size = size / 1024
+	}
+	return fmt.Sprintf("%.1f%s", float64(size), units[6])
 }
-if !flag || size < 1024 {
-return fmt.Sprintf("%d%s", size, units[0])
+func FilterFiles(path string, all bool) []os.DirEntry {
+	selected := []os.DirEntry{}
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("Ошибка: ", err)
+		return nil
+	}
+	if all {
+		return entries
+	}
+	for _, entry := range entries {
+		if !strings.HasPrefix(entry.Name(), ".") {
+			selected = append(selected, entry)
+		}
+	}
+	return selected
 }
-size = size / 1024
-for i := 1; i < 6; i++ {
-if size < 1024 {
-return fmt.Sprintf("%.1f%s", float64(size), units[i])
-}
-size = size / 1024
-}
-return fmt.Sprintf("%.1f%s", float64(size), units[6])
-}
-func GetPathSize(path string, human bool) (string, error) {
-info, err := os.Lstat(path)
-if err != nil {
-return "", err
-}
-if info.IsDir() {
-entries, _ :=os.ReadDir(path)
-var dirSize int64 = 0
-for _, entry := range entries {
-entryInfo, _ := entry.Info()
-dirSize += entryInfo.Size()
-}
-return ConvertSize(dirSize, human), nil
-}
-return ConvertSize(info.Size(), human), nil
+
+func GetPathSize(path string, human, all bool) (string, error) {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return "", err
+	}
+	if info.IsDir() {
+		entries := FilterFiles(path, all)
+		var dirSize int64 = 0
+		for _, entry := range entries {
+			entryInfo, _ := entry.Info()
+			dirSize += entryInfo.Size()
+		}
+		return ConvertSize(dirSize, human), nil
+	}
+	return ConvertSize(info.Size(), human), nil
 }
